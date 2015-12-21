56a5025b473c51e9dc4d82bfc1d9df9748a1973a
Alexander Chernaev <achernaev@setelis.com>
2015-12-21 15:29:06 +0200

    Started http://chimera.labs.oreilly.com/books/1234000000754/ch15.html

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,9 +3,13 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.utils.html import escape
+from unittest import skip
 
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 from lists.models import Item, List
 
 
@@ -90,13 +94,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_ivalid_input()
-        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_ivalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         item1 = Item.objects.create(list=list_, text="textey")
@@ -105,7 +108,7 @@ class ListViewTest(TestCase):
             data={"text": "textey"}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, "list.html")
         self.assertEqual(Item.objects.all().count(), 1)
@@ -113,8 +116,12 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get("/lists/%d/" % (list_.id))
-        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_ivalid_input()
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
 
 
 class NewListTest(TestCase):
